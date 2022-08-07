37cbd8230cc0464ef6a5fd0c71aa7c1bbe548cc4
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-08-07 21:45:44 +0800

    Create about.js

---
+++ src/commands/about.js
@@ -0,0 +1,22 @@
+const { MessageEmbed, version } = require('discord.js');
+module.exports = {
+    name: 'about',
+    execute: async ({ interaction, axios, Embed, client }) => {
+        await interaction.deferReply();
+        try {
+         about = (await axios.get(`https://brawley.js.org/about.json`)).data;
+        }catch(e) {}
+      if(!about) return await interaction.editReply({
+        embeds: [new MessageEmbed().setDescription(`Hey ${interaction?.user?.username}, I couldn't fetch information about myself at this moment, can you please visit [My Website](https://brawley.js.org) instead.`)]
+      })
+        const embed = (Embed({ ...about?.embed }));
+        embed.setAuthor(`${about?.greet ? about?.greet[Math.floor(Math.random() * about?.greet?.length)] : 'Hello There'} ${interaction?.user?.username}`, interaction?.user?.avatarURL({ dynamic: true, format: 'png', size: 1024 }));
+        embed.setThumbnail(client?.user?.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
+        embed.setTitle(`Jet Brawley`)
+        embed.setDescription(`${about?.description.split("{discord.js-version}").join(version).split("{uptime}").join(`<t:${Math.floor(client.readyAt / 1000)}> (<t:${Math.floor(client.readyAt / 1000)}:R>)`)}`)
+        embed.setTimestamp()
+        await interaction.editReply({
+            embeds: [embed]
+        });
+    }
+}
