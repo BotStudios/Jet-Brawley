ba3a4079088b1730c9125cb534c3d1e179063e7c
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-08-07 21:50:20 +0800

    Create cocSave.js

---
+++ src/commands/cocSave.js
@@ -0,0 +1,84 @@
+const { MessageEmbed } = require('discord.js');
+module.exports = {
+    name: 'coc-save',
+    execute: async ({ interaction, fetchAPI, cocDB, useEmote, filterTag }) => {
+        try {
+        var tag = interaction.options.getString('tag');
+        var db = await cocDB.findOne({ user: `${interaction?.user?.id}` });
+        await interaction.deferReply({
+            ephermal: true
+        });
+        if(!tag && !db) return await interaction.editReply({
+            embeds: [
+                new MessageEmbed()
+                    .setDescription(`
+                       \`Please provide a Clash Of Clans in-game player tag to save\`
+                    `)
+                    .setImage(`https://brawley.is-a.dev/assets/savecoctag.png`)
+            ],
+            ephermal: true
+        });
+        if(!tag && db) return await interaction.editReply({
+            embeds: [
+                new MessageEmbed()
+                    .setDescription(`
+                    \`To overwite your current saved tag, run this command again with another in-game tag\`
+
+                    **Your Current Tag:** \`${decodeURIComponent(db?.tag)}\`
+                    `)
+                    .setImage(`https://brawley.is-a.dev/assets/savecoctag.png`)
+                    
+            ],
+            ephermal: true
+        });
+        const data = await fetchAPI({
+            type: 'coc',
+            endpoint: `/players/${filterTag(tag)}`
+        });
+        if(data?.name && !db) {
+            await cocDB({
+            user: `${interaction?.user?.id}`,
+            tag: `${filterTag(tag)}`
+        }).save();
+           return await interaction.editReply({
+            embeds: [
+                new MessageEmbed()
+                    .setTitle('Successfully saved !')
+                    .setDescription(`Your in-game tag (\`${tag}\`) was saved !`)
+            ],
+            ephermal: true
+           });
+        }
+        if(data?.name && db) {
+           db.delete().then(async () => {
+            await cocDB({
+                user: `${interaction?.user?.id}`,
+                tag: `${filterTag(tag)}`
+            }).save()
+        });
+        return await interaction.editReply({
+            embeds: [
+                new MessageEmbed()
+                    .setTitle('Successfully saved !')
+                    .setDescription(`Your in-game tag (\`${tag}\`) was saved & overwrited !`)
+            ],
+            ephermal: true
+           });
+        }
+            return await interaction.editReply({
+                embeds: [
+                    new MessageEmbed()
+                        .setDescription(`This tag (\`${tag}\`) does not exist !`)
+                ], ephermal: true
+            });
+    
+}catch(e) {
+    return await interaction.editReply({
+        embeds: [
+            new MessageEmbed()
+                .setDescription(`This tag (\`${tag}\`) does not exist !`)
+        ], ephermal: true
+    });
+}
+    }
+}
