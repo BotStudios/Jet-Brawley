c0d59cfadb6d68f3c0fed738cbfc70b826adf100
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-11-19 16:11:58 +0800

    Update about.js

--- src/commands/about.js
+++ src/commands/about.js
@@ -10,7 +10,7 @@ module.exports = {
         embeds: [new EmbedBuilder().setDescription(`Hey ${interaction?.user?.username}, I couldn't fetch information about myself at this moment, can you please visit [My Website](https://brawley.js.org) instead.`)]
       })
         const embed = (msgembed(about?.embed));
-        embed.setAuthor(`${about?.greet ? about?.greet[Math.floor(Math.random() * about?.greet?.length)] : 'Hello There'} ${interaction?.user?.username}`, interaction?.user?.avatarURL({ dynamic: true, format: 'png', size: 1024 }));
+        embed.setAuthor({ name: `${about?.greet ? about?.greet[Math.floor(Math.random() * about?.greet?.length)] : 'Hello There'} ${interaction?.user?.username}`, iconURL: interaction?.user?.avatarURL({ dynamic: true, format: 'png', size: 1024 }) });
         embed.setThumbnail(client?.user?.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
         embed.setTitle(`Jet Brawley`)
         embed.setDescription(`${about?.description.split("{discord.js-version}").join(version).split("{uptime}").join(`<t:${Math.floor(client.readyAt / 1000)}> (<t:${Math.floor(client.readyAt / 1000)}:R>)`)}`)
