4eda1328733053c3111bc06324127d8c17b906a3
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-11-19 16:05:53 +0800

    Update about.js

--- src/commands/about.js
+++ src/commands/about.js
@@ -1,15 +1,15 @@
-const { MessageEmbed, version } = require('discord.js');
+const { EmbedBuilder, version } = require('discord.js');
 module.exports = {
     name: 'about',
-    execute: async ({ interaction, axios, Embed, client }) => {
+    execute: async ({ interaction, axios, msgembed, client }) => {
         await interaction.deferReply();
         try {
          about = (await axios.get(`https://brawley.js.org/about.json`)).data;
         }catch(e) {}
       if(!about) return await interaction.editReply({
-        embeds: [new MessageEmbed().setDescription(`Hey ${interaction?.user?.username}, I couldn't fetch information about myself at this moment, can you please visit [My Website](https://brawley.js.org) instead.`)]
+        embeds: [new EmbedBuilder().setDescription(`Hey ${interaction?.user?.username}, I couldn't fetch information about myself at this moment, can you please visit [My Website](https://brawley.js.org) instead.`)]
       })
-        const embed = (Embed({ ...about?.embed }));
+        const embed = (msgembed({ ...about?.embed }));
         embed.setAuthor(`${about?.greet ? about?.greet[Math.floor(Math.random() * about?.greet?.length)] : 'Hello There'} ${interaction?.user?.username}`, interaction?.user?.avatarURL({ dynamic: true, format: 'png', size: 1024 }));
         embed.setThumbnail(client?.user?.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
         embed.setTitle(`Jet Brawley`)
