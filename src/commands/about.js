4bb5eeb208270b740031757f7a8d1633dd083f7f
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-11-19 16:08:34 +0800

    Update about.js

--- src/commands/about.js
+++ src/commands/about.js
@@ -9,7 +9,7 @@ module.exports = {
       if(!about) return await interaction.editReply({
         embeds: [new EmbedBuilder().setDescription(`Hey ${interaction?.user?.username}, I couldn't fetch information about myself at this moment, can you please visit [My Website](https://brawley.js.org) instead.`)]
       })
-        const embed = (msgembed({ ...about?.embed }));
+        const embed = (msgembed(about?.embed));
         embed.setAuthor(`${about?.greet ? about?.greet[Math.floor(Math.random() * about?.greet?.length)] : 'Hello There'} ${interaction?.user?.username}`, interaction?.user?.avatarURL({ dynamic: true, format: 'png', size: 1024 }));
         embed.setThumbnail(client?.user?.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
         embed.setTitle(`Jet Brawley`)
