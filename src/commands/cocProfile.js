8c19281edae88b353c5f13903e662548ff86fa55
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-11-19 16:14:45 +0800

    Update cocProfile.js

--- src/commands/cocProfile.js
+++ src/commands/cocProfile.js
@@ -1,10 +1,10 @@
-const { MessageEmbed, MessageActionRow, MessageButton, MessageAttachment } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, MessageAttachment } = require('discord.js');
 const { createCanvas, loadImage } = require('canvas');
 
 
 module.exports = {
     name: 'coc-profile',
-    execute: async ({ interaction, fetchAPI, cocDB, filterTag, useEmote, delay, Embed }) => {
+    execute: async ({ interaction, fetchAPI, cocDB, filterTag, useEmote, delay, msgembed }) => {
                var tag = interaction.options.getString('tag') ? encodeURIComponent(interaction.options.getString('tag')) : '';
         var user = interaction.options.getUser('user') ? (interaction.options.getUser('user'))?.id : '';
         var player;
@@ -13,10 +13,10 @@ module.exports = {
         if(!tag) tag = (await cocDB.findOne({ user: `${interaction.user.id}` }))?.tag;
         if(user) tag = (await cocDB.findOne({ user: `${user}` }))?.tag;
         if(user && !tag) return interaction.editReply({
-            embeds: [ new MessageEmbed().setDescription('This user does not have a saved tag.') ]
+            embeds: [ new EmbedBuilder().setDescription('This user does not have a saved tag.') ]
         });
         if(!tag) return interaction.editReply({
-                    embeds: [ new MessageEmbed().setDescription(`You haven't save an in-game tag for Clash Of Clans`)],
+                    embeds: [ new EmbedBuilder().setDescription(`You haven't save an in-game tag for Clash Of Clans`)],
         });
         const data = await fetchAPI({
             type: 'coc',
@@ -25,7 +25,7 @@ module.exports = {
         const { name, trophies, townHallLevel, expLevel, troops, spells, heroes, bestTrophies, attackWins, defenseWins, builderHallLevel, versusTrophies, versusBattleWins, donations, donationsReceived, clan } = data;
         const playerTag = data?.tag;
     
-        const profileEmbed = (Embed())
+        const profileEmbed = (msgembed())
         .setTitle(`${name} | ${decodeURIComponent(playerTag)} ${useEmote('smilebs')}`)
         .setAuthor({ name: `${interaction.user.tag}`, icon: interaction.user.avatarURL({ dynamic: true, format: 'png', size: 1024 }) })
         .setColor('#7c8cf7')
@@ -56,7 +56,7 @@ module.exports = {
     }catch(e) {
         await interaction.editReply({
             embeds: [
-                new MessageEmbed()
+                new EmbedBuilder()
                 .setDescription('Something Went Wrong !')
             ]
         })
