f44db4f0b6abfc8fd93caf35a753eced324d71f8
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-11-19 13:21:38 +0800

    Update crProfile.js

--- src/commands/crProfile.js
+++ src/commands/crProfile.js
@@ -5,10 +5,16 @@ const { createCanvas, loadImage } = require('canvas');
 module.exports = {
     name: 'cr-profile',
     execute: async ({ interaction, fetchAPI, crDB, filterTag, useEmote, delay, Embed }) => {
-        var tag = interaction.options.getString('tag') ? encodeURIComponent(interaction.options.getString('tag')) : '';
+               var tag = interaction.options.getString('tag') ? encodeURIComponent(interaction.options.getString('tag')) : '';
+        var user = interaction.options.getUser('user') ? (interaction.options.getUser('user'))?.id : '';
+        var player;
         try {
         await interaction.deferReply();
         if(!tag) tag = (await crDB.findOne({ user: `${interaction.user.id}` }))?.tag;
+        if(user) tag = (await crDB.findOne({ user: `${user}` }))?.tag;
+        if(user && !tag) return interaction.editReply({
+            embeds: [ new MessageEmbed().setDescription('This user does not have a saved tag.') ]
+        });
         if(!tag) return interaction.editReply({
                     embeds: [ new MessageEmbed().setDescription(`You haven't save an in-game tag for Clash Royale`)],
         });
@@ -23,21 +29,21 @@ module.exports = {
         .setTitle(`${name} | ${decodeURIComponent(playerTag)} ${useEmote('smilebs')}`)
         .setAuthor({ name: `${interaction.user.tag}`, icon: interaction.user.avatarURL({ dynamic: true, format: 'png', size: 1024 }) })
         .setColor('#7c8cf7')
-        .addField(`**Trophies**`, `${useEmote('trophy')} ${trophies ? trophies : 0 }`, true)
-        .addField(`**Best Trophies**`, `${useEmote('trophy')} ${bestTrophies ? bestTrophies : 0}`, true)
-        .addField('\u200B', '_ _', true)
-       .addField('**Level**', `${useEmote('xp')} ${expLevel ? expLevel :0} - ${expPoints}`, true)
-       .addField(`**Wins/Losses/Total**`, `${useEmote('soloshowdown')} ${wins ? wins : 0}/${losses ? losses : 0}/${battleCount ? battleCount : 0}`, true)
-        .addField('\u200B', '_ _', true)
-       .addField(`**Three Crowns**`, `${useEmote('duoshowdown')} ${threeCrownWins ? threeCrownWins : 0}`, true)
-       .addField(`**Total Donations**`, `${useEmote('3v3')} ${totalDonations ? totalDonations : 0}`, true)
-        .addField('\u200B', '_ _', true)
-       .addField('**Cards Unlocked**', `${useEmote('brawler')} ${cards ? cards.length : 0}`, true)
-       .addField('**Arena**', `${useEmote('brawler')} ${arena ? arena?.name : 0}`, true)
-       .addField('\u200B', '_ _', true) 
-       .addField('**Favorite Card**', `${currentFavouriteCard ? currentFavouriteCard.name : 'None'}`, true)
-       .addField('**Clan**', `${useEmote('club')} ${clan ? `${clan.name} - ${clan.tag}` : 'Not In A Clan'}`, true)
-          .addField('\u200B', '_ _', true)
+        .addFields({ name: `**Trophies**`, value: `${useEmote('trophy')} ${trophies ? trophies : 0 }`, inline: true })
+        .addFields({ name: `**Best Trophies**`, value: `${useEmote('trophy')} ${bestTrophies ? bestTrophies : 0}`, inline: true })
+        .addFields({ name: '\u200B', value: '_ _', inline: true })
+       .addFields({ name: '**Level**', value: `${useEmote('xp')} ${expLevel ? expLevel :0} - ${expPoints}`, inline: true })
+       .addFields({ name: `**Wins/Losses/Total**`, value: `${useEmote('soloshowdown')} ${wins ? wins : 0}/${losses ? losses : 0}/${battleCount ? battleCount : 0}`, inline: true })
+        .addFields({ name: '\u200B', value: '_ _', inline: true })
+       .addFields({ name: `**Three Crowns**`, value: `${useEmote('duoshowdown')} ${threeCrownWins ? threeCrownWins : 0}`, inline: true })
+       .addFields({ name: `**Total Donations**`, value: `${useEmote('3v3')} ${totalDonations ? totalDonations : 0}`, inline: true })
+        .addFields({ name: '\u200B', value: '_ _', inline: true })
+       .addFields({ name: '**Cards Unlocked**', value: `${useEmote('brawler')} ${cards ? cards.length : 0}`, inline: true })
+       .addFields({ name: '**Arena**', value: `${useEmote('brawler')} ${arena ? arena?.name : 0}`, inline: true })
+       .addFields({ name: '\u200B', value: '_ _', inline: true }) 
+       .addFields({ name: '**Favorite Card**', value: `${currentFavouriteCard ? currentFavouriteCard.name : 'None'}`, inline:true })
+       .addFields({ name: '**Clan**', value: `${useEmote('club')} ${clan ? `${clan.name} - ${clan.tag}` : 'Not In A Clan'}`,inline: true})
+          .addFields({ name: '\u200B', value: '_ _', inline: true })
        .setTimestamp();
 
        const canvas = createCanvas(1085, 183);
