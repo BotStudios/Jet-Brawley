418522bcfa5509df2c88172d866a15d6370c276f
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-11-19 16:07:06 +0800

    Update bsMap.js

--- src/commands/bsMap.js
+++ src/commands/bsMap.js
@@ -1,8 +1,8 @@
-const { MessageEmbed, MessageActionRow, MessageAttachment } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, AttachmentBuilder } = require('discord.js');
 const { createCanvas, loadImage } = require('canvas');
 module.exports = {
     name: 'bs-map',
-    execute: async ({ interaction, useEmote, axios, cache, Embed }) => {
+    execute: async ({ interaction, useEmote, axios, cache, msgembed }) => {
         await interaction.deferReply();
         const name = interaction.options.getString('name');
         if(!name) return;
@@ -11,25 +11,26 @@ module.exports = {
             maps = (await axios.get(`https://api.brawlapi.com/v1/maps`))?.data?.list;
             cache.set('maps', maps);
         }
-        var map = maps?.find(m => m?.name?.toLowerCase() === name?.toLowerCase());
+        var map = maps?.find(m => m?.name?.toLowerCase().split(" ").join("") ===name?.toLowerCase().split(" ").join(""));
         if(!map) return await interaction.editReply({
-            embeds: [new MessageEmbed().setDescription(`Couldn't find map \`${name}\`.`)]
+            embeds: [new EmbedBuilder().setDescription(`Couldn't find map \`${name}\`.`)]
         })
-        const embed = (Embed()).setTitle(`${map?.name}`).setColor('#34eb6b').setTimestamp();
+        const embed = (msgembed()).setTitle(`${map?.name}`).setColor('#34eb6b').setTimestamp();
         try {
         const image = await loadImage(map?.imageUrl);
         const canvas = createCanvas(image.width, image.height);
         const ctx = canvas.getContext('2d');
         ctx.drawImage(image, 0, 0);
-        const attach = new MessageAttachment(await canvas.createPNGStream(), 'map.png');
+        const attach = new AttachmentBuilder(await canvas.createPNGStream(), { name: 'map.png' });
         embed.setImage('attachment://map.png');
         await interaction.editReply({
             embeds: [embed],
             files: [attach]
         });
         }catch(e) {
+          console.log(e)
             await interaction.editReply({
-                embeds: [new MessageEmbed().setDescription(`Couldn't find map \`${name}\`.`)],
+                embeds: [new EmbedBuilder().setDescription(`Couldn't find map \`${name}\`.`)],
             });
         }
         }
